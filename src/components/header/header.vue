c86d96a3896fe9a96572f6a27da54ed7223b5346
bing.zhub <bing.zhub@gmail.com>
2019-04-12 23:10:21 +0800

    finish inflate data

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -4,6 +4,19 @@
       <div class="avatar">
         <img height="64" width="64" :src="seller.avatar"/>
       </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}</span>
+        </div>
+        <div class="description">
+          {{seller.description}}/{{seller.deliveryTime}}分钟送达
+        </div>
+        <div v-if="seller.supports" class="support" >
+          <span class="icon"></span>
+          <span class="text">{{seller.supports[0].description}}</span>
+        </div>
+      </div>
     </div>
     <div class="buletin-wrapper">
 
