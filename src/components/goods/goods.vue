073966722453c933d1083d821b540d7b557242d9
bing.zhub <bing.zhub@gmail.com>
2019-04-24 15:57:11 +0800

    finish food-item no scroll

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -9,7 +9,32 @@
               </li>
         </ul>
     </div>
-    <div class="foods-wrapper"></div>
+    <div class="foods-wrapper">
+      <ul>
+        <li v-for="(item, index) in goods" :key="index">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="(food, index) in item.foods" class="food-item" :key="index">
+              <div class="icon">
+                <img :src="food.icon" width="57px" height="57px">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.decription}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now">¥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -37,7 +62,20 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-@import url('../../common/stylus/common.styl')
+border-1px($color)
+  position: relative;
+  &:after
+    display: block;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    border-top: 1px solid $color;
+    content: ' ';
+
+border-none()
+  &.after
+    display: none
 
 .goods
     display: flex
@@ -90,5 +128,52 @@ export default {
             content: ' ';
   .foods-wrapper
     flex: 1
-
+    .title
+      padding-left: 14px
+      height: 26px
+      line-height: 26px
+      border-left: 2px solid #d9dde1
+      font-size: 12px
+      color: rgb(147, 153, 159)
+      background: #f3f5f7
+    .food-item
+      display: flex
+      margin: 18px
+      border-1px(rgba(7, 17, 27, 0.1))
+      padding-bottom: 18px
+      &:last-child
+        border-none()
+        margin-bottom: 0
+      .icon
+        flex: 0 0 57px
+        margin-right: 10px
+      .content
+        flex: 1
+        .name
+          margin: 2px 0 8px 0
+          height: 14px
+          line-height: 14px
+          font-size: 14px
+          color: rgb(7, 17, 27)
+        .desc, .extra
+          line-height: 10px
+          font-size: 10px
+          color: rgb(147, 153, 159)
+        .desc
+          margin-bottom: 8px
+        .extra
+          line-height: 10px
+          &.count
+            margin-right: 12px
+        .price
+          font-weight: 700
+          line-height: 24px
+          .now
+            margin-right: 8px
+            font-size: 14px
+            color: rgb(240, 20, 20)
+          .old
+            text-decoration: line-through
+            font-size: 10px
+            color: rgb(147, 153, 159)
 </style>
