fc8881611ba327a7f65e3239b97a90eacef408bc
bing.zhub <bing.zhub@gmail.com>
2019-04-26 21:19:51 +0800

    finish rating div

--- src/components/rating/rating.vue
+++ src/components/rating/rating.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="rating">
-      <div class="type">
-          <span>{{desc.all}}</span>
-          <span>{{desc.positive}}</span>
-          <span>{{desc.negative}}</span>
+      <div class="type border-1px">
+          <span class="block positive" :class="{'active': selectType===2}">{{desc.all}}<span class="count">47</span></span>
+          <span class="block positive" :class="{'active': selectType===0}">{{desc.positive}}<span class="count">47</span></span>
+          <span class="block negative" :class="{'active': selectType===1}">{{desc.negative}}<span class="count">47</span></span>
       </div>
       <div class="swirch">
           <span class="icon-check_circle"></span>
@@ -47,4 +47,44 @@ export default {
 }
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+border-1px($color)
+    position: relative;
+    &:after
+        display: block;
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        border-top: 1px solid $color;
+        content: ' ';
+
+  .rating
+    .type
+      padding: 18px
+      margin: 0 18px
+      border-1px(rgb(7, 17, 27, 0.1))
+      font-size: 0
+      .block
+        border-radius: 2px
+        margin-right: 8px
+        color: #fff 
+        font-size: 12px
+        display: inline-block
+        padding: 8px 12px
+        color: rgb(77, 85, 93)
+        &.active
+          color: #fff  
+        .count
+          font-size: 8px
+          margin-left: 2px
+          line-height: 16px  
+        &.positive
+          background: rgba(0, 160, 220, 0.2)
+          &.active
+            background: rgb(0, 160, 220)
+        &.negative
+          background: rgba(77, 85, 93, 0.2)
+          &.active
+            background: rgb(77, 85, 93)
+</style>
